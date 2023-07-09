fb34cad18c93d1c8ea20294e7beacc52d2b3fde1
Bruno Braz <araujobraz.alexandre@gmail.com>
2023-07-08 23:52:23 -0300

    Primeiro commit

---
+++ src/hello/world/Program.java
@@ -0,0 +1,11 @@
+package hello.world;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!!!");
+		System.out.println("Teste github!!");
+	}
+
+}
